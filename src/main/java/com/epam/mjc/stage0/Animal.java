a3181db0f7ef780c7f11de102d9037e1cb69977c
Shoghik <shoghik.kachatryan@dci-student.org>
2023-10-22 14:15:38 +0200

    first commit

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,19 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String c, int n, boolean h) {
+        color = c;
+        numberOfPaws = n;
+        hasFur = h;
+    }
+
+    public String getDescription() {
+        return String.format("This animal is mostly %s. It has %d %s and "
+                + "%s fur.", color, numberOfPaws, (numberOfPaws == 1 ? "paw" : "paws")
+                , (hasFur? "a" : "no"));
+    }
 }
---
+++ src/main/java/com/epam/mjc/stage0/Main.java
@@ -0,0 +1,12 @@
+package com.epam.mjc.stage0;
+
+public class Main {
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+        Dog dog = new Dog();
+
+        System.out.println(bird.getDescription());
+        System.out.println(dog.getDescription());
+
+    }
+}
